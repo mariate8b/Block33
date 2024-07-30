fdd09964793e7430847928bb3d7a1931d7eb4cec
MARIA JARAMILLO <mariajaramillo@MARIAs-MacBook-Pro.local>
2024-07-30 16:26:02 -0500

    last update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const pg = require('pg');
+const port = 3000;
 
 
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_hr_directory')
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_hr_directory');
+
+const server = express()
 
 const init = async() =>{
  await client.connect();
@@ -10,14 +13,14 @@ const init = async() =>{
 
  let SQL =`
  DROP TABLE IF EXISTS employees;
- DROP TABLE IF EXISTS categories;
+ DROP TABLE IF EXISTS deparment;
 
- CREATE TABLE Department(
+ CREATE TABLE department(
  id SERIAL PRIMARY KEY 
  name VARCHAR(255) NOT NULL
  );
 
- CREATE TABLE Employee(
+ CREATE TABLE employees(
  id SERIAL PRIMARY KEY
  name VARCHAR(255) NOT NULL,
  created_at TIMESTAMP DEFAULT now(),
@@ -31,6 +34,29 @@ const init = async() =>{
 await client.query(SQL);
 console.log("tables created");
 
-}
+SQL= `
+INSERT INTO departments (name) VALUES
+('Engineering'),
+('Marketing'),
+('Sales');
+
+INSERT INTO employees (txt, ranking, department_id)
+VALUES ('create tables', 18, (SELECT id FROM departments WHERE name = 'Engineering'));
+
+INSERT INTO employees (txt, ranking, department_id)
+VALUES ('create tables', 5, (SELECT id FROM departments WHERE name = 'Marketing'));
+
+INSERT INTO employees (txt, ranking, department_id)
+VALUES ('create tables', 187, (SELECT id FROM departments WHERE name = 'Sales'));
+
+`
+await client.query(SQL);
+console.log("seeded tables");
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT,() => {
+console.log(`Server listening on port ${PORT}`);
+});
+};
 
 init()
